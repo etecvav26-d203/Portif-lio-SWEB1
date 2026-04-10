2e70705449027f406996bd188406e3c691e13177
df732025-tech <df732025@gmail.com>
2026-04-10 18:36:07 -0300

    Add files via upload

---
+++ Daniel-Enzo/Ex.01/ex.01/calcula.php
@@ -0,0 +1,28 @@
+<?php
+
+$nv = $_GET["nv"];
+
+if($nv >= 100 && $nv <= 200){
+    echo"O numero esta entre esses valores";
+}else{
+    echo"O numero nao esta entre esses valores";
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+<title> Verificado</title>
+
+</head>
+
+<body>
+
+<a href="index.php">Voltar</a>
+
+</body>
+
+</html>
---
+++ Daniel-Enzo/Ex.01/ex.08/index8.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 8</title>
+</head>
+<body>
+
+    <h1>Soma de 0 até N</h1>
+
+    <form action="calcula.php" method="POST">
+        <label for="numero">Digite um número inteiro:</label>
+        <input type="number" id="numero" name="n" required>
+
+        <br><br>
+
+        <input type="submit" value="Mostrar soma">
+    </form>
+
+</body>
+</html>
